f725461b75cda2dba3df5a6d613697b04b8ad193
Chavhare Mayuri Sudam <144438613+mayurichavhare2004@users.noreply.github.com>
2024-10-10 06:47:53 -0700

    Add files via upload
    hi

---
+++ Moon.java
@@ -0,0 +1,13 @@
+package chapter4;
+public class Moon extends Earth {
+    public static void main(String args[])
+    {
+        SolarSystem s = new SolarSystem();
+        Earth e = new Earth();
+        Mars m = new Mars();
+
+        System.out.println(s instanceof SolarSystem);
+        System.out.println(e instanceof Earth);
+        System.out.println(m instanceof SolarSystem);
+    }
+}
---
+++ SolarSystem.java
@@ -0,0 +1,7 @@
+package chapter4;
+class SolarSystem {
+}
+class Earth extends SolarSystem {
+}
+class Mars extends SolarSystem {
+}
